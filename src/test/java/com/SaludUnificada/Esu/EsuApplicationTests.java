fc286a8db6377b486f46808a067cb1264e203a28
Ricardocejas <ricardoalfredocejas97@gmail.com>
2026-05-29 23:54:47 -0300

    Estructura inicial sprint boot

---
+++ src/test/java/com/SaludUnificada/Esu/EsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SaludUnificada.Esu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
